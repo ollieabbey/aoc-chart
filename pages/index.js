15974a6e6c1d8b337325980e21b347c50e797b77
Ollie Abbey <oliver.abbey@sky.uk>
2020-12-10 19:34:55 +0000

    Fetched data as part of getStaticProps call

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({ data, error }) {
+  const getDataForPerson = name => {
+    let key
+    for (key in data.members) {
+      const member = data.members[key]
+      if (member.name === name) {
+        return member
+      }
+    }
+    return `${name} not found!`
+  }
+  const name = 'Ollie Abbey'
+  const errorText = error && `Error: ${error}`
   return (
     <div className={styles.container}>
       <Head>
@@ -11,55 +23,42 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Identity Advent of Code Leaderboard
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <p className={styles.description}>
+          {name}:
+          <code className={styles.code}>{JSON.stringify(getDataForPerson(name))}</code>
+        </p>
+        <p className={styles.description}>
+          {errorText}
+        </p>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const session = process.env.AOC_SESSION
+  if (!session) {
+    return {
+      props: {
+        error: 'No Advent of Code session provided.',
+        data: {
+          members: {}
+        }
+      }
+    }
+  }
+  const response = await fetch("https://adventofcode.com/2020/leaderboard/private/view/496748.json", {
+    headers: {
+      cookie: `session=${session}`
+    },
+  });
+  const data = await response.json()
+  return {
+    props: {
+      data,
+    }
+  }
 }
